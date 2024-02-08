7893959b01c4ace9f43ffdcf69b104599077aa2e
murat <murat@adana01.de>
2024-02-08 20:10:32 +0100

    finish

--- vanilla-web/warmup-countdown/script.js
+++ vanilla-web/warmup-countdown/script.js
@@ -1,12 +1,27 @@
 const h1El = document.querySelector('h1');
 const timerEl = document.querySelector('#timer');
+const beepAudio = document.querySelector('#beepAudio');
+const beepEnd = document.querySelector('#beepEnd');
+
+h1El.textContent = 'pause';
+timerEl.textContent = 8;
+console.log(h1El.textContent);
 
 
-const textPause = () => {h1El.innerText = `pause`};
-const textAktive = () => {h1El.innerText = `aktive phase`};
+setInterval (()=>{
+    timerEl.textContent;
+    timerEl.textContent--
+    if (timerEl.textContent === '3'){
+        beepAudio.play();
+    }
+    if (timerEl.textContent === '0'){
+        beepEnd.play();
+    }
+    if(timerEl.textContent === '-1' && h1El.textContent === 'pause') {
+        h1El.textContent = 'aktive';
+        timerEl.textContent = '15'
+    } else if (timerEl.textContent === '-1' && h1El.textContent === 'aktive') {
+        h1El.textContent = 'pause';
+        timerEl.textContent = '8'}
+},1000)
 
-function startTimer () {
-    if (h1El.innerText === 'pause') {
-        
-}
-}
